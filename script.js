61cb5b3a129ea00873efa253076043b79bd60e8a
SimonBesendorfer <simon.besendorfer@me.com>
2020-10-10 17:07:52 +0200

    added bootstrap

--- script.js
+++ script.js
@@ -1,2 +1,37 @@
+const BASE_SERVER_URL = 'http://simon-besendorfer.developerakademie.com/PHP_DD/';
 let doctors = [];
 
+function load() {
+    loadJSONFromServer()
+        .then(function (result) { //then(function (variable vom server))
+            console.log('Laden erfolgreich!', result);
+            myJSON = JSON.parse(result);
+            
+        })
+        .catch(function (error) { // Fehler
+            console.error('Fehler beim laden!', error);
+        });
+}
+
+function loadJSONFromServer() {
+    return new Promise(function (resolve, reject) {
+        let xhttp = new XMLHttpRequest();
+        let proxy = 'https://cors-anywhere.herokuapp.com/';
+        let serverURL = proxy + BASE_SERVER_URL + 'doctors.json';
+        xhttp.open('GET', serverURL);
+
+        xhttp.onreadystatechange = function (oEvent) {
+            if (xhttp.readyState === 4) {
+                if (xhttp.status >= 200 && xhttp.status <= 399) {
+                    resolve(xhttp.responseText);
+                } else {
+                    reject(xhttp.statusText);
+                }
+            }
+        };
+
+        xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhttp.send();
+
+    });
+}
